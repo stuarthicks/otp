505a71928b083f1f8d9adf824475aa1cf09c52f1
Stuart Hicks <stuarthicks@users.noreply.github.com>
2020-02-22 19:47:23 +0000

    refresh project to stave off code rot

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 extern crate base32;
 extern crate oath;
 
+use oath::{totp_raw_now, HashType};
+
 static SECRET: &'static str = env!("SECRET");
 
 fn main() {
-    let length = 6;
-    let offset = 0;
-    let steps = 30;
-
-    let key = base32::decode(base32::Alphabet::RFC4648 {padding: false}, SECRET).unwrap();
-    println!("{:06}", oath::totp_raw(key.as_ref(), length, offset, steps));
+    let key = base32::decode(base32::Alphabet::RFC4648 { padding: false }, SECRET);
+    match key {
+        Some(v) => println!("{:?}", totp_raw_now(v.as_ref(), 6, 0, 30, &HashType::SHA1)),
+        None => println!("Failed to base32 decode the totp secret"),
+    }
 }
